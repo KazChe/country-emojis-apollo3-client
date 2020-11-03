ead3c1a3d6b467b9bcfef1ec31d6e9bf88dd0d3f
KazChe <kaz.che@gmail.com>
2020-11-03 15:43:18 -0800

    refactor useQuery to its own method

--- src/App.js
+++ src/App.js
@@ -13,22 +13,28 @@ const COUNTRY = gql`
 
 function App() {
   const [ code, setCode] = useState("")
-  const {data, loading, error } = useQuery(COUNTRY, {
-    variables: { code },
-    skip: code.length !== 2
-  })
-
+  const data = Feed(code)
   const handleChange = (e) => {
     setCode(e.target.value)
   }
   return (
     <div className="App">
-      {error  && (<h1>{`You broke it ${error.message}`}</h1>)}
-      {!data || loading ? (<h1>Loading...</h1>) :
+      { !data ? <h1>Loading...</h1> :
           (<h1>{data.country?.name} {data.country?.emoji}</h1>)}
       <input type="text" value={code} onChange={handleChange}/>
     </div>
   );
+}
+
+function Feed(code) {
+  const {loading, error, data} = useQuery(COUNTRY, {
+    variables: { code },
+    skip: code.length !== 2
+  })
+  if (error) return error;
+  if (loading || !data ) return "Loading...";
+
+  return data;
 }
 
 export default App;
