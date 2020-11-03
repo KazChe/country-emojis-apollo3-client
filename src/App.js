b57ee9bb8d3c437d2e7cef368a90e7ddb90872be
KazChe <kaz.che@gmail.com>
2020-11-03 15:52:24 -0800

    reverting back to original qay od using useQuery

--- src/App.js
+++ src/App.js
@@ -13,28 +13,22 @@ const COUNTRY = gql`
 
 function App() {
   const [ code, setCode] = useState("")
-  const data = Feed(code)
+  const {data, loading, error } = useQuery(COUNTRY, {
+    variables: { code },
+    skip: code.length !== 2
+  })
+
   const handleChange = (e) => {
     setCode(e.target.value)
   }
   return (
     <div className="App">
-      { !data ? <h1>Loading...</h1> :
+      {error  && (<h1>{`You broke it ${error.message}`}</h1>)}
+      {!data || loading ? (<h1>Loading...</h1>) :
           (<h1>{data.country?.name} {data.country?.emoji}</h1>)}
       <input type="text" value={code} onChange={handleChange}/>
     </div>
   );
-}
-
-function Feed(code) {
-  const {loading, error, data} = useQuery(COUNTRY, {
-    variables: { code },
-    skip: code.length !== 2
-  })
-  if (error) return error;
-  if (loading || !data ) return "Loading...";
-
-  return data;
 }
 
 export default App;
